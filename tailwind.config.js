fdfc8b1d2166a1a5877e38bfcb765478698ec6bd
ThaBeanBoy <gtchipoyera@gmail.com>
2022-11-14 01:20:30 +0200

    Finished touching up the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,28 @@ module.exports = {
     './src/components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    screens: {
+      mobile: '375px',
+      desktop: '1440px',
+    },
+
+    colors: {
+      // Primary
+      'very-dark-blue': 'hsl(243, 87%, 12%)',
+      'desaturated-blue': 'hsl(238, 22%, 44%)',
+      // Accent
+      'bright-blue': 'hsl(224, 93%, 58%)',
+      'moderate-cyan': 'hsl(170, 45%, 43%)',
+      // Neutral
+      'light-grayish-blue': 'hsl(240, 75%, 98%)',
+      'light-gray': 'hsl(0, 0%, 75%)',
+    },
+
+    fontFamily: {
+      'open-sans': ['Open Sans', 'sans-serif'],
+      raleway: ['Raleway', 'sans-serif'],
+    },
+
     extend: {},
   },
   plugins: [],
