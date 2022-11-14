1f615b8efbead6e2495673663e101e6efb8ad327
ThaBeanBoy <gtchipoyera@gmail.com>
2022-11-14 11:41:35 +0200

    Finalised branch

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Layout from '../components/layout';
 
 const IndexPage = () => {
   return <></>;
@@ -7,4 +6,20 @@ const IndexPage = () => {
 
 export default IndexPage;
 
-export const Head = () => <title>Home Page</title>;
+export const Head = () => {
+  return (
+    <>
+      <meta charset='UTF-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='32x32'
+        href='./images/favicon-32x32.png'
+      />
+
+      <title>Frontend Mentor | Fylo landing page with two column layout</title>
+    </>
+  );
+};
