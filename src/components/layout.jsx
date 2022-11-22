26107504b631759668ec0b661109f9a1ddd2d1d1
ThaBeanBoy <gtchipoyera@gmail.com>
2022-11-22 19:09:32 +0200

    Just finished the mobile version of the footer, bout to install react icons

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,9 +2,6 @@ import React, { useState } from "react";
 import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 
-// import logo from "./logo.svg";
-import close from "./close.svg";
-
 const links = [
   {
     title: "Features",
@@ -54,7 +51,6 @@ function Layout({ children }) {
       <div id="wrapper" className="sticky top-0 left-0 bg-[white]">
         <header className="mx-auto flex w-screen max-w-[1280px] items-center justify-between py-[32px] px-[16px] min-desktop-width:px-[32px]">
           <Link to="/">
-            {/* <img src={logo} alt="logo image" className="sm:w-auto w-[116px]" /> */}
             <StaticImage
               src="../images/logo.svg"
               className="w-[116px] min-desktop-width:w-auto"
@@ -82,18 +78,140 @@ function Layout({ children }) {
 
       <main>{children}</main>
 
+      <footer className="bg-very-dark-blue px-[16px] py-[96px]">
+        <StaticImage src="../images/logo-footer.svg" className="mb-[60px]" />
+
+        <main className="flex flex-col items-start">
+          <div id="social-links" className="mb-[92px] text-[white]">
+            <a
+              href="tel:+1-543-123-4567"
+              alt="phone"
+              className="mb-[32px] flex items-center"
+            >
+              <StaticImage
+                src="../images/icon-phone.svg"
+                className="mr-[28px]"
+              />
+              Phone: +1-543-123-4567
+            </a>
+            <a
+              href="mailto:example@fylo.com"
+              alt="email"
+              className="flex items-center"
+            >
+              <StaticImage
+                src="../images/icon-email.svg"
+                className="mr-[28px]"
+              />
+              Phone: +1-543-123-4567
+            </a>
+          </div>
+
+          <div id="link-groups" className="mb-[72px]">
+            {[
+              [
+                {
+                  title: "about us",
+                  link: "/about-us",
+                },
+                {
+                  title: "jobs",
+                  link: "/jobs",
+                },
+                {
+                  title: "press",
+                  link: "/press",
+                },
+                {
+                  title: "blog",
+                  link: "/blog",
+                },
+              ],
+              [
+                {
+                  title: "contact us",
+                  link: "/contact-us",
+                },
+                {
+                  title: "terms",
+                  link: "/terms",
+                },
+                {
+                  title: "privacy",
+                  link: "/privacy",
+                },
+              ],
+            ].map((group) => (
+              <div id="group" className="mb-[80px] last:mb-0">
+                {group.map(({ title, link }) => (
+                  <Link
+                    to={link}
+                    className="mb-[32px] block text-[white] last:mb-0"
+                  >
+                    {title}
+                  </Link>
+                ))}
+              </div>
+            ))}
+          </div>
+
+          <div id="social-icons" className="flex w-[100%] justify-center">
+            <a href="#">
+              <StaticImage
+                src="../images/icon-facebook.svg"
+                alt="facebook"
+                width={40}
+                height={40}
+              />
+              <StaticImage
+                src="../images/icon-twitter.svg"
+                alt="facebook"
+                className="mx-[16px]"
+                width={40}
+                height={40}
+              />
+              <StaticImage
+                src="../images/icon-instagram.svg"
+                alt="facebook"
+                width={40}
+                height={40}
+              />
+            </a>
+          </div>
+        </main>
+        <p class="mt-[32px] text-center text-[white]">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            className="text-moderate-cyan"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a href="#" className="text-moderate-cyan">
+            Your Name Here
+          </a>
+          .
+        </p>
+      </footer>
+
       {showMobileNav ? (
         <div
           id="mobile-nav-wrapper"
           className="fixed top-0 left-0 z-10 h-screen w-screen bg-light-gray/50 min-desktop-width:hidden"
-          onClick={() => console.log("outside clicked")}
         >
           <main className="absolute top-0 right-0 flex h-screen min-w-[200px] flex-col items-end bg-light-grayish-blue pt-[32px] pr-[32px]">
             <button
               className="mb-[32px]"
               onClick={() => setShowMobileNav(false)}
             >
-              <img src={close} alt="close image" width="32px" height="32px" />
+              <StaticImage
+                src="../images/close.svg"
+                alt="close image"
+                width={32}
+                height={32}
+              />
             </button>
 
             <ul className="flex flex-col items-end">{NavLinks}</ul>
